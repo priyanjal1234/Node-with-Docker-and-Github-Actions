5fbfee00ee7b876574fa9488c9b2adc7c493cc73
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-04-26 09:13:29 +0530

    Automating Deployment of Node with docker to dockerhub with Github Actions

---
+++ index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/",function(req,res) {
+    res.send("Hello World")
+})
+
+
+const port = process.env.PORT || 4000
+app.listen(port,function() {
+    console.log(`Server is running on port ${port}`)
+})
